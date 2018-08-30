2ec2578c8e839f010d95d4effffd25ce12927132
nikitakumar2017 <41698630+nikitakumar2017@users.noreply.github.com>
2018-08-30 21:01:06 +0530

    assignment11

--- assignment11.py
+++ assignment11.py
@@ -1,6 +1,19 @@
 #Q.1- Write a python code to find a valid email address from a text.
-^[a-zA-Z][a-zA-Z0-9_.]*[@](gmail.com|yahoo.com)
+import re
+string=input("Enter a string from which you want to extract email ")
+s=re.search('[a-zA-Z][a-zA-Z0-9_.]*[@](gmail.com|yahoo.com)',string)
+if s!=None:
+    print('Match Available ');
+    print('First occurence is at: {},End: {},Pattern found: {}'.format(s.start(),s.end(),s.group()))
+else:
+    print('Match not available')
 
 #Q.2- Write a python program to find a valid Indian phone number from a text.
 #(Valid Indian numbers will start with "+91-" and after that [6-9] followed by 9 digits. )
-+91-[6-9][0-9]{9}
+import re
+string=input("Enter a string from which you want to extract phone no. ")
+matcher=re.finditer('\+91-[6-9][0-9]{9}',string)
+for m in matcher:
+    print("Match is at: {},End:{},Group found: {} ".format(m.start(),m.end(),m.group()))
+
+
